b54e953ae3d09b467b989178afe83c0d3811f52a
jassem-lab <jassemgaaloul123@gmail.com>
2020-04-17 04:10:43 +0200

    Timer Application with React Class Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Timer from './component/Timer';
+function App() {
+  return (
+   <div className= "app">
+   
+    <Timer/>
+   </div>
+  );
+}
+
+export default App;
